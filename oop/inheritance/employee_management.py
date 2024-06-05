d8ab9cdd56b7e5cb888375bcea9150e0a3ccaf46
greatbrain <erickfania@gmail.com>
2024-06-05 11:44:46 -0400

    Inheritance exercise

--- oop/inheritance/employee_management.py
+++ oop/inheritance/employee_management.py
@@ -6,14 +6,20 @@ class Employee:
           self._name = name
           self._id = emp_id
           self.salary = salary
-
       
       # Common method implemented for ervery type of employee and will be overwrited.
-      def get_employee_work(sef):
+      def get_actual_work(sef):
           pass
+
+      def get_rise(self, percentage):
+          self.salary += self.salary * percentage / 100 
 
 
 # Manager employee
 class Manager(Employee):
-      pass
+      def __init__(self, name, emp_id, salary, team_size):
+          super().__init__(name, emp_id, salary)
+          self.team_size = team_size
 
+      def get_actual_work(self):
+          return f"{self.name} is managing a team of developers"
